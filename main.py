8e29a679793791a068dfb90e7f2dd19053c0f764
Artem <artnicanov@gmail.com>
2022-11-02 00:19:46 +0300

    correct import added, all methods checked

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import data
--- views.py
+++ views.py
@@ -1,68 +1,72 @@
-import main
+from main import *
+import json
+from flask import Flask, request
 
 # вьюшка для получения всех пользователей
-@main.app.route('/users')
+@app.route('/users')
 def get_users():
-	user_list = main.User.query.all()  # к классу User применяем атрибут для запроса, а далее метод для получения всех элементов
+	user_list = User.query.all()  # к классу User применяем атрибут для запроса, а далее метод для получения всех элементов
 	users_result = []
 	for user in user_list:
 		users_result.append(user.user_dict())
 	# возвращаем результат в виде json словаря, применяя метод для модели
-	return main.jsonify(users_result)
+	return jsonify(users_result)
+
+
 
 # вьюшка для получения пользователя по id
-@main.app.route("/users/<int:id>")
+@app.route("/users/<int:id>")
 def get_user_by_id(id):
-	user = main.User.query.get(id)  # к классу User применяем атрибут для запроса, а далее метод для получения элемента
+	user = User.query.get(id)  # к классу User применяем атрибут для запроса, а далее метод для получения элемента
 	if user is None:
 		return 'user not found'
 	# возвращаем результат в виде json словаря, применяя метод для модели
-	return main.jsonify(user.user_dict())
+	return jsonify(user.user_dict())
 
 # вьюшка для получения всех заказов
-@main.app.route('/orders')
+@app.route('/orders')
 def get_orders():
-	order_list = main.Order.query.all()  # к классу Order применяем атрибут для запроса, а далее метод для получения всех элементов
+	order_list = Order.query.all()  # к классу Order применяем атрибут для запроса, а далее метод для получения всех элементов
 	orders_result = []
 	for order in order_list:
 		orders_result.append(order.order_dict())
 	# возвращаем результат в виде json словаря, применяя метод для модели
-	return main.jsonify(orders_result)
+	return jsonify(orders_result)
 
 # вьюшка для получения заказа по id
-@main.app.route("/orders/<int:id>")
+@app.route("/orders/<int:id>")
 def get_order_by_id(id):
-	order = main.Order.query.get(id)  # к классу Order применяем атрибут для запроса, а далее метод для получения элемента
+	order = Order.query.get(id)  # к классу Order применяем атрибут для запроса, а далее метод для получения элемента
 	if order is None:
 		return 'order not found'
 	# возвращаем результат в виде json словаря, применяя метод для модели
-	return main.jsonify(order.order_dict())
+	return jsonify(order.order_dict())
 
 # вьюшка для получения всех предложений
-@main.app.route('/offers')
+@app.route('/offers')
 def get_offers():
-	offer_list = main.Offer.query.all()  # к классу Offer применяем атрибут для запроса, а далее метод для получения всех элементов
+	offer_list = Offer.query.all()  # к классу Offer применяем атрибут для запроса, а далее метод для получения всех элементов
 	offers_result = []
 	for offer in offer_list:
 		offers_result.append(offer.offer_dict())
 	# возвращаем результат в виде json словаря, применяя метод для модели
-	return main.jsonify(offers_result)
+	return jsonify(offers_result)
 
 # вьюшка для получения предложения по id
-@main.app.route("/offers/<int:id>")
+@app.route("/offers/<int:id>")
 def get_offer_by_id(id):
-	offer = main.Offer.query.get(id)  # к классу Offer применяем атрибут для запроса, а далее метод для получения элемента
+	offer = Offer.query.get(id)  # к классу Offer применяем атрибут для запроса, а далее метод для получения элемента
 	if offer is None:
 		return 'offer not found'
 	# возвращаем результат в виде json словаря, применяя метод для модели
-	return main.jsonify(offer.offer_dict())
+	return jsonify(offer.offer_dict())
 
 # вьюшка для добавления пользователя
-@main.app.route("/users", methods=["POST"])
+@app.route("/users", methods=["POST"])
 def add_user():
-	user_data = main.json.loads(main.request.data)  #  то что передается в запросе, формируется в json формате
+	user_data = json.loads(request.data)  #  то что передается в запросе, формируется в json формате
 	#  создаем экземпляр класса User на основе переданного запроса
-	new_user = main.User(
+	new_user = User(
 		id = user_data["id"],
 		first_name = user_data["first_name"],
 		last_name = user_data["last_name"],
@@ -71,39 +75,39 @@ def add_user():
 		role = user_data["role"],
 		phone = user_data["phone"]
 	)
-	main.db.session.add(new_user)  # добавляем объект db в сессию через атрибут session, а к нему применяем метод для добавления пользователя
-	main.db.session.commit()  # подтверждаем сессию
+	db.session.add(new_user)  # добавляем объект db в сессию через атрибут session, а к нему применяем метод для добавления пользователя
+	db.session.commit()  # подтверждаем сессию
 	return ""
 
 # вьюшка для обновления или удаления пользователя по id
-@main.app.route("/users/<int:id>", methods=["PUT", "DELETE"])
+@app.route("/users/<int:id>", methods=["PUT", "DELETE"])
 def change_or_delete_user(id):
-	if main.request.method == "PUT":
-		user_data = main.json.loads(main.request.data)  # то что передается в запросе, формируется в json формате
-		new_user = main.User.query.get(id)  # к классу User применяем атрибут для запроса, а далее метод для получения элемента
-		new_user.first_name = user_data["first_name"],
-		new_user.last_name = user_data["last_name"],
-		new_user.age = user_data["age"],
-		new_user.email = user_data["email"],
-		new_user.role = user_data["role"],
+	if request.method == "PUT":
+		user_data = json.loads(request.data)  # то что передается в запросе, формируется в json формате
+		new_user = User.query.get(id)  # к классу User применяем атрибут для запроса, а далее метод для получения элемента
+		new_user.first_name = user_data["first_name"]
+		new_user.last_name = user_data["last_name"]
+		new_user.age = user_data["age"]
+		new_user.email = user_data["email"]
+		new_user.role = user_data["role"]
 		new_user.phone = user_data["phone"]
 
-		main.db.session.add(new_user)
-		main.db.session.commit()
+		db.session.add(new_user)
+		db.session.commit()
 		return ""
 
-	if main.request.method == "DELETE":
-		new_user = main.User.query.get(id)
-		main.db.session.delete(new_user)
-		main.db.session.commit()
+	if request.method == "DELETE":
+		new_user = User.query.get(id)
+		db.session.delete(new_user)
+		db.session.commit()
 		return ""
 
 # вьюшка для добавления заказа
-@main.app.route("/orders", methods=["POST"])
+@app.route("/orders", methods=["POST"])
 def add_order():
-	order_data = main.json.loads(main.request.data)  #  то что передается в запросе, формируется в json формате
+	order_data = json.loads(request.data)  #  то что передается в запросе, формируется в json формате
 	#  создаем экземпляр класса Order на основе переданного запроса
-	new_order = main.Order(
+	new_order = Order(
 		id=order_data["id"],
 		name=order_data["name"],
 		description=order_data["description"],
@@ -113,46 +117,68 @@ def add_order():
 		customer_id=order_data["customer_id"],
 		executor_id=order_data["executor_id"]
 	)
-	main.db.session.add(new_order)  # добавляем объект db в сессию через атрибут session, а к нему применяем метод для добавления пользователя
-	main.db.session.commit()  # подтверждаем сессию
+	db.session.add(new_order)  # добавляем объект db в сессию через атрибут session, а к нему применяем метод для добавления пользователя
+	db.session.commit()  # подтверждаем сессию
 	return ""
 
 # вьюшка для обновления или удаления заказа по id
-@main.app.route("/orders/<int:id>", methods=["PUT", "DELETE"])
+@app.route("/orders/<int:id>", methods=["PUT", "DELETE"])
 def change_or_delete_order(id):
-	# подготовить для PUT
+	if request.method == "PUT":
+		order_data = json.loads(request.data)  # то что передается в запросе, формируется в json формате
+		new_order = Order.query.get(id)  # к классу Order применяем атрибут для запроса, а далее метод для получения элемента
+		new_order.id = order_data["id"]
+		new_order.name = order_data["name"]
+		new_order.description = order_data["description"]
+		new_order.start_date = order_data["start_date"]
+		new_order.end_date = order_data["end_date"]
+		new_order.address = order_data["address"]
+		new_order.customer_id = order_data["customer_id"]
+		new_order.executor_id = order_data["executor_id"]
 
-	if main.request.method == "DELETE":
-		new_order = main.Order.query.get(id)
-		main.db.session.delete(new_order)
-		main.db.session.commit()
+		db.session.add(new_order)
+		db.session.commit()
+		return ""
+
+	if request.method == "DELETE":
+		new_order = Order.query.get(id)
+		db.session.delete(new_order)
+		db.session.commit()
 		return ""
 
 # вьюшка для добавления предложения
-@main.app.route("/offers", methods=["POST"])
+@app.route("/offers", methods=["POST"])
 def add_offer():
-	offer_data = main.json.loads(main.request.data)  #  то что передается в запросе, формируется в json формате
+	offer_data =json.loads(request.data)  #  то что передается в запросе, формируется в json формате
 	#  создаем экземпляр класса User на основе переданного запроса
-	new_offer = main.Offer(
+	new_offer = Offer(
 		id=offer_data["id"],
 		order_id=offer_data["order_id"],
 		executor_id=offer_data["executor_id"]
 	)
-	main.db.session.add(new_offer)  # добавляем объект db в сессию через атрибут session, а к нему применяем метод для добавления пользователя
-	main.db.session.commit()  # подтверждаем сессию
+	db.session.add(new_offer)  # добавляем объект db в сессию через атрибут session, а к нему применяем метод для добавления пользователя
+	db.session.commit()  # подтверждаем сессию
 	return ""
 
 # вьюшка для обновления или удаления предложения по id
-@main.app.route("/offers/<int:id>", methods=["PUT", "DELETE"])
+@app.route("/offers/<int:id>", methods=["PUT", "DELETE"])
 def change_or_delete_offer(id):
-	# подготовить для PUT
+	if request.method == "PUT":
+		offer_data = json.loads(request.data)  # то что передается в запросе, формируется в json формате
+		new_offer = Offer.query.get(id)  # к классу Order применяем атрибут для запроса, а далее метод для получения элемента
+		new_offer.id = offer_data["id"]
+		new_offer.order_id = offer_data["order_id"]
+		new_offer.executor_id = offer_data["executor_id"]
 
-	if main.request.method == "DELETE":
-		new_offer = main.Offer.query.get(id)
-		main.db.session.delete(new_offer)
-		main.db.session.commit()
+		db.session.add(new_offer)
+		db.session.commit()
+		return ""
+
+	if request.method == "DELETE":
+		new_offer = Offer.query.get(id)
+		db.session.delete(new_offer)
+		db.session.commit()
 		return ""
 
 
-
-main.app.run()
+app.run()
